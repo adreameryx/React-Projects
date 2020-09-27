933c2de89b33870b2558bd7633bbd4e91542adf1
adreameryx <mt3178@srmist.edu.in>
2020-09-27 23:32:59 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import { link } from 'react-router-dom';
 
 function navbar() {
+    const {click, setClick} = useState(false);
+
+    const handleClick = () => setClick(!click);
     return (
         <>
          <nav className="navbar">
@@ -9,11 +12,14 @@ function navbar() {
                  <link to="/" className="navbar-logo">
                      Travello! <i className="fab fa-typo3"></i>
                  </link>
+                 <div className='menu-icon' onClick={handleClick}>
+                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                 </div>
              </div>
              
         </nav>   
         </>
-    )
+    );
 }
 
 export default navbar
